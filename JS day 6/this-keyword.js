6c9e357198491e65dc56393944b21fab060ab370
Praagya007 <praagyadevkota@gmail.com>
2025-08-28 16:13:51 +0545

    Source code containing this keyword basics + usage of call, apply and bind

---
+++ JS day 6/this-keyword.js
@@ -0,0 +1,32 @@
+// this Basics + call/apply/bind
+// ---------------------
+function introduce(city, country) {
+    console.log(`Hi, I'm ${this.name} from ${city}, ${country}`);
+}
+
+let user1 = { name: "Bob" };
+introduce.call(user1, "Kathmandu", "Nepal");
+
+let user2 = { name: "Xin" };
+introduce.apply(user2, ["Beijing", "China"]);
+
+let user3 = { name: "Garry" };
+let userInfo = introduce.bind(user3, "Edgebaston", "England");
+userInfo();
+
+// ---------------------
+// Fun Exercise with call/apply/bind
+// ---------------------
+function sayHello(greeting) {
+    console.log(`${greeting}, I am ${this.name}`);
+}
+
+let userA = { name: "Alice" };
+sayHello.call(userA, "Namaste");
+
+let userB = { name: "Bob" };
+sayHello.apply(userB, ["Hello"]);
+
+let userC = { name: "Carlos" };
+let greetingUserC = sayHello.bind(userC, "Hola");
+greetingUserC();
